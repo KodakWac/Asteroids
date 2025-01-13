b05d74784caa005559f01bed51b7c944508a6b6a
KodakWac <mylifeis11111@gmail.com>
2025-01-12 17:17:20 -0800

    D: made game window/.gitignore file

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame
+import sys
 from constants import *
 
 def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False  
+
+        screen.fill((0, 0, 0))  
+        pygame.display.flip()  
+
+    pygame.quit()  
+    sys.exit()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 
 if __name__ == "__main__":
