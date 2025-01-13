9b86a9ea6c7689a8b112faa899d4862fbdaafe37
KodakWac <mylifeis11111@gmail.com>
2025-01-12 19:14:48 -0800

    G: added player class to game screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from player import Player
 from constants import *
 
 def main():
@@ -7,6 +8,8 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -17,6 +20,7 @@ def main():
                 running = False 
 
         screen.fill((0, 0, 0))  
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000   
 
